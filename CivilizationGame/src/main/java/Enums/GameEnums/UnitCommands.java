5db079d2fb2e96c0d1e7249ff548ae3091b44f6a
Mahdi Akbar <mahdiakbr81@gmail.com>
2022-05-14 16:55:55 +0430

    Add the cocmmand for attacking a city

--- CivilizationGame/src/main/java/Enums/GameEnums/UnitCommands.java
+++ CivilizationGame/src/main/java/Enums/GameEnums/UnitCommands.java
@@ -4,7 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum UnitCommands {
-    UNIT_MOVE_TO("unit move to -i (?<x>\\d+) -j (?<y>\\d+)"); //TODO fill enums
+    UNIT_MOVE_TO("unit move to -i (?<x>\\d+) -j (?<y>\\d+)"),
+    UNIT_ATTACK("unit attack city"); //TODO fill enums
     private final String regex;
 
     UnitCommands(String regex) {
