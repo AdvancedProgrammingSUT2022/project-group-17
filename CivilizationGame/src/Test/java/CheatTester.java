b4f192161fd5bc3c7e6120fbbfb100b9fe3b4677
Amir-H-ravan <amir.h.rnn@gmail.com>
2022-05-16 21:25:59 +0430

    final ?

--- CivilizationGame/src/Test/java/CheatTester.java
+++ CivilizationGame/src/Test/java/CheatTester.java
@@ -7,6 +7,7 @@ import Model.Nations.Nation;
 import Model.Nations.NationType;
 import Model.Users.User;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import java.util.Scanner;
@@ -20,6 +21,8 @@ public class CheatTester extends Tester {
         user.setNation(new Nation(NationType.PERSIA));
         Game.setSubTurn(0);
         Game.getPlayersInGame().add(user);
+        Game.getPlayersInGame().add(new User("","",""));
+        Game.getPlayersInGame().get(1).setNation(new Nation(NationType.PERSIA));
         Game.getPlayersInGame().get(Game.getSubTurn()).setNation(new Nation(NationType.PERSIA));
         Game.setLoggedInUser(user);
         CheatController.setCurrentTurnUser(user);
@@ -103,5 +106,42 @@ public class CheatTester extends Tester {
         commandMatcher = CheatCommands.getMatcher("destroy improvement on -x 3 -y 3",CheatCommands.DESTROY_IMPROVEMENT);
         if (commandMatcher.matches())
             Assert.assertEquals("The improvement is now broken!",cheatController.destroyImprovement(commandMatcher));
+    }
+
+    @Test
+    public void cheatIncreaseCoin(){
+        commandMatcher = CheatCommands.getMatcher("increase --gold 100",CheatCommands.CHEAT_INCREASE_GOLD);
+        if (commandMatcher.matches())
+            Assertions.assertEquals("100 coins added successfully",cheatController.increaseCoin(commandMatcher));
+    }
+//    @Test
+//    public void cheatIncreaseTurn(){
+//        commandMatcher = CheatCommands.getMatcher("increase --turn 1",CheatCommands.CHEAT_INCREASE_TURN);
+//        if (commandMatcher.matches())
+//            Assertions.assertEquals("Progress through 1 turns successfully",cheatController.increaseTurn(commandMatcher));
+//    }
+    @Test
+    public void cheatIncreaseFood(){
+        commandMatcher = CheatCommands.getMatcher("increase --food 100",CheatCommands.CHEAT_INCREASE_FOOD);
+        if (commandMatcher.matches())
+            Assertions.assertEquals("100 food added successfully",cheatController.increaseFood(commandMatcher));
+    }
+    @Test
+    public void cheatIncreaseProduction(){
+        commandMatcher = CheatCommands.getMatcher("increase --production 100",CheatCommands.CHEAT_INCREASE_PRODUCTION);
+        if (commandMatcher.matches())
+            Assertions.assertEquals("100 production added successfully",cheatController.increaseProduction(commandMatcher));
+    }
+    @Test
+    public void cheatIncreaseHappiness(){
+        commandMatcher = CheatCommands.getMatcher("increase --happiness 100",CheatCommands.CHEAT_INCREASE_HAPPINESS);
+        if (commandMatcher.matches())
+            Assertions.assertEquals("100 happiness added successfully",cheatController.increaseHappiness(commandMatcher));
+    }
+    @Test
+    public void cheatWon(){
+        commandMatcher = CheatCommands.getMatcher("win game",CheatCommands.CHEAT_WIN);
+        if (commandMatcher.matches())
+            Assertions.assertEquals("You won",cheatController.winGame());
     }
 }
