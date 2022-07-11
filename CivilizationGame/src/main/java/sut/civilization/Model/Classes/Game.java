d3cdc3b1c59313b11272ed403710e3ef747f7e35
hamedsy <hamed007.saboor@gmail.com>
2022-07-11 14:28:45 +0430

    Almost complete infoPanels

--- CivilizationGame/src/main/java/sut/civilization/Model/Classes/Game.java
+++ CivilizationGame/src/main/java/sut/civilization/Model/Classes/Game.java
@@ -124,6 +124,7 @@ public class Game {
 
         switch (menu) {
             case GAME_MENU:
+                fxmlLoader.setLocation(Civilization.class.getResource("fxml/infoPanel.fxml"));
                 break;
             case CHAT_MENU:
                 fxmlLoader.setLocation(Civilization.class.getResource("fxml/ChatMenu.fxml"));
