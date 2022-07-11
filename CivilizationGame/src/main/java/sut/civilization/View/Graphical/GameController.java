d3166ab2c76504535bbea1ebf14d22ad51e172be
Mahdi Akbar <mahdiakbr81@gmail.com>
2022-07-11 15:44:12 +0430

    Can Choose nations and opponents and start a game

--- CivilizationGame/src/main/java/sut/civilization/View/Graphical/GameController.java
+++ CivilizationGame/src/main/java/sut/civilization/View/Graphical/GameController.java
@@ -1,23 +1,28 @@
 package sut.civilization.View.Graphical;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import sut.civilization.Enums.Menus;
+import sut.civilization.Model.Classes.Building;
 import sut.civilization.Model.Classes.Game;
+import sut.civilization.Model.Classes.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GameController {
     private final sut.civilization.Controller.GameControllers.GameController gameController = new sut.civilization.Controller.GameControllers.GameController();
     public Label chooseNation1;
-    public TextField chooseOpponents1;
     public ChoiceBox nations1;
-    public TextField chooseOpponents2;
-    public Label chooseNation2;
-    public ChoiceBox nations2;
+    public ScrollPane scrollPane;
+
+    public ArrayList<String> opponents = new ArrayList<>();
+    public HashMap<String, ChoiceBox> selectedNations = new HashMap<>();
 
 
     public void mainMenu(MouseEvent mouseEvent) {
@@ -27,25 +32,34 @@ public class GameController {
     public void startGame(MouseEvent mouseEvent) {
         if (canGameStart()){
             int nation_one = Integer.parseInt(String.valueOf(nations1.getValue().toString().charAt(0)));
-            System.out.println(nation_one);
+            Game.getPlayersInGame().add(Game.getLoggedInUser());
             gameController.chooseNation(nation_one, 0);
-            int nation_two = Integer.parseInt(String.valueOf(nations2.getValue().toString().charAt(0)));
-            gameController.chooseNation(nation_two, 1);
-            System.out.println(Game.getPlayersInGame().size());
+            int i = 1;
+            for (String opponent : opponents) {
+                Game.getPlayersInGame().add(gameController.getUserByName(opponent));
+                int nationNumber = Integer.parseInt(String.valueOf((selectedNations.get(opponent).getValue().toString().charAt(0))));
+                gameController.chooseNation(nationNumber, i++);
+            }
+            for (User user : Game.getPlayersInGame()) {
+                System.out.println(user.getNation().getNationType().name);
+                System.out.println(user.getUsername());
+            }
+            Game.changeScene(Menus.GAME_MENU);
         }
     }
 
     public void loadGame(MouseEvent mouseEvent) {
+
     }
 
     public boolean canGameStart(){
-        int opponentsNumber1 = Integer.parseInt(chooseOpponents1.getText());
-        int opponentsNumber2 = Integer.parseInt(chooseOpponents2.getText());
-        if (opponentsNumber1 < 0 || opponentsNumber1 > 3)
-            return false;
-        if (opponentsNumber2 < 0 || opponentsNumber2 > 3)
-            return false;
+//        if (nations1.getValue().equals(null)){
+//            System.out.println("ridi");
+//            return false;
+//        }
 
+        if (opponents.size() == 0)
+            return false;
         return true;
     }
 
@@ -55,7 +69,38 @@ public class GameController {
         for (int i = 0; i < 9; i++) {
             Label label = new Label(nations[i]);
             nations1.getItems().add(label.getText());
-            nations2.getItems().add(label.getText());
         }
+
+
+        HBox hBox = new HBox();
+        VBox usernames = new VBox();
+        VBox opponentsNations = new VBox();
+        usernames.setPrefWidth(200);
+        usernames.setPrefHeight(400);
+        for (User user : Game.getUsers()) {
+            Button button = new Button(user.getUsername());
+            button.setPrefHeight(50);
+            button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    button.setStyle("-fx-background-color: red");
+                    opponents.add(button.getText());
+                }
+            });
+            usernames.getChildren().add(button);
+
+            ChoiceBox choiceBox = new ChoiceBox<>();
+            choiceBox.setPrefHeight(50);
+            for (int i = 0; i < 9; i++) {
+                Label label = new Label(nations[i]);
+                choiceBox.getItems().add(label.getText());
+            }
+            opponentsNations.getChildren().add(choiceBox);
+            selectedNations.put(user.getUsername(), choiceBox);
+        }
+
+        hBox.getChildren().add(usernames);
+        hBox.getChildren().add(opponentsNations);
+        scrollPane.setContent(hBox);
     }
 }
--- CivilizationGame/src/main/java/sut/civilization/View/Graphical/MainController.java
+++ CivilizationGame/src/main/java/sut/civilization/View/Graphical/MainController.java
@@ -10,7 +10,7 @@ public class MainController extends ViewController{
     sut.civilization.Controller.MainController mainController = new sut.civilization.Controller.MainController();
 
     public void startGame(MouseEvent mouseEvent) {
-        Game.changeScene(Menus.GAME_MENU);
+        Game.changeScene(Menus.SELECTION_MENU);
     }
 
     public void enterProfileMenu(MouseEvent mouseEvent) {
