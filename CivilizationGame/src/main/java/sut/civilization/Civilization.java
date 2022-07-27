59955cf389f63240dcbd5427a4a1565b27780a97
Mahdi Akbar <mahdiakbr81@gmail.com>
2022-07-27 15:57:42 +0430

    Add music

--- CivilizationGame/src/main/java/sut/civilization/Civilization.java
+++ CivilizationGame/src/main/java/sut/civilization/Civilization.java
@@ -3,6 +3,8 @@ package sut.civilization;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 import sut.civilization.Enums.Menus;
@@ -14,6 +16,9 @@ public class Civilization extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Media media = new Media(getClass().getResource("/sut/civilization/Music/Theme.mp3").toExternalForm());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
         Game.instance.setCurrentScene(new Scene(Game.instance.loadScene(Menus.LOGIN_MENU)));
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("Images/Icons/programIcon.png")).toExternalForm()));
         stage.setScene(Game.instance.getCurrentScene());
